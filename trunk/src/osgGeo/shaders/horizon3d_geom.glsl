d88b9f473680ad8021d7008132343acb2189354f
kristofer.tingdahl@dgbes.com <kristofer.tingdahl@dgbes.com@1a978a74-06dc-1005-337c-a6ef347e0f4e>
2012-10-08 08:32:40 +0000

    Added svn:keyword attribute, and file headers where missing.

--- trunk/src/osgGeo/shaders/horizon3d_geom.glsl
+++ trunk/src/osgGeo/shaders/horizon3d_geom.glsl
@@ -1,4 +1,22 @@
-#version 330 compatibility
+# version 330 compatibility
+# osgGeo - A collection of geoscientific extensions to OpenSceneGraph.
+# Copyright 2011 dGB Beheer B.V. and others.
+# 
+# osgGeo is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Lesser General Public License for more details.
+# 
+# You should have received a copy of the GNU Lesser General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>
+# 
+# $Id$
+#
 
 layout( triangles ) in;
 layout( triangle_strip, max_vertices = 3 ) out;
