8042d4c8b46ff490b821c6745a30929ddf34e8a6
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2012-10-08 07:19:26 +0000

    Made compile.

--- src/osgGeo/PolyLine.cpp
+++ src/osgGeo/PolyLine.cpp
@@ -1,20 +1,44 @@
+/* osgGeo - A collection of geoscientific extensions to OpenSceneGraph.
+Copyright 2011 dGB Beheer B.V.
+
+osgGeo is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+*/
+
+
+#include <osgGeo/Polyline>
+
+#include <osg/Geometry>
+#include <osgUtil/CullVisitor>
+
 namespace osgGeo
 {
 
 PolylineNode::PolylineNode()
     : radius_( 5 )
-    , maxRadius( -1 )
+    , maxRadius_( -1 )
     , screenSizeScaling_( false )
     , arrayModifiedCount_( 0 )
 {}
 
 
-PolylineNode::PolylineNode( const PolylineNode& node )
-    : radius_( node.radius_ )
+PolylineNode::PolylineNode( const PolylineNode& node, const osg::CopyOp& co )
+    : osg::Node( node, co )
+    , radius_( node.radius_ )
     , maxRadius_( node.maxRadius_ )
     , screenSizeScaling_( node.screenSizeScaling_ )
     , array_( node.array_ )
-    , geometry_( node.geometry_->clone() )
+    , geometry_( (osg::Geometry*) node.geometry_->clone(co) )
     , arrayModifiedCount_( 0 )
 {}
 
@@ -32,14 +56,14 @@ void PolylineNode::traverse( osg::NodeVisitor& nv )
 	    }
 	}
     }
-    else if ( nv.getVisitorType()==osg::NodeVisitor::CULL_VISTOR )
+    else if ( nv.getVisitorType()==osg::NodeVisitor::CULL_VISITOR )
     {
 	if ( geometry_.valid() )
 	{
 	    osgUtil::CullVisitor* cv = dynamic_cast<osgUtil::CullVisitor*>(&nv);
 
 	    if ( getStateSet() )
-		cv->pushStateSet();
+		cv->pushStateSet( getStateSet() );
 
 	    cv->addDrawable( geometry_, cv->getModelViewMatrix() );
 
