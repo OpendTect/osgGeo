36f748e7442a996ac2989e5a37dd87d16f61016f
kristofer.tingdahl@dgbes.com <kristofer.tingdahl@dgbes.com@1a978a74-06dc-1005-337c-a6ef347e0f4e>
2012-10-08 07:19:32 +0000

    Fixed typo.

--- trunk/src/osgGeo/PolyLine.cpp
+++ trunk/src/osgGeo/PolyLine.cpp
@@ -16,7 +16,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 
 
-#include <osgGeo/Polyline>
+#include <osgGeo/PolyLine>
 
 #include <osg/Geometry>
 #include <osgUtil/CullVisitor>
