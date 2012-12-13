dce6b288006a6a315a9cc3ffb364de76fc5aa6eb
ranojay.sen@dgbes.com <ranojay.sen@dgbes.com@1a978a74-06dc-1005-337c-a6ef347e0f4e>
2012-12-13 11:40:36 +0000

    improved PolyLines for supporting ZStretch

--- trunk/examples/PolyLines.cpp
+++ trunk/examples/PolyLines.cpp
@@ -24,8 +24,12 @@ $Id$
 #include <osg/Node> 
 #include <osgGeo/PolyLine>
 #include <osgViewer/Viewer> 
+#include <osg/MatrixTransform>
+#include <osgGA/TrackBallManipulator>
+#include <osg/Material>
 
-osg::ref_ptr<osg::Node> drawDNA()
+
+osg::ref_ptr<osg::Node> drawDNA(float xoffset, float zscale, const osg::Vec4& col )
 {
     srand( clock() );
     osg::ref_ptr<osg::Vec3Array> coords = new osg::Vec3Array;
@@ -37,23 +41,25 @@ osg::ref_ptr<osg::Node> drawDNA()
 	new osg::DrawElementsUInt(osg::PrimitiveSet::TRIANGLE_STRIP, 0);
 
     int ci = 0;
-    for ( int idx=0; idx<150; idx++ )
+    for ( int idx=0; idx<200; idx++ )
     { 
-	osg::Vec3 coord( 200*sin(idx/10.0), 200*cos(idx/10.0), idx*20 ); 
+	osg::Vec3 coord( xoffset + 200*sin(idx/10.0), 200*cos(idx/10.0), idx*20*zscale ); 
 	coords->push_back( coord );
 	primset1->push_back( ci++ );
     }
     
-    for ( int idx=0; idx<150; idx++ )
+    for ( int idx=0; idx<200; idx++ )
     { 
-	osg::Vec3 coord( -200*sin(idx/10.0), -200*cos(idx/10.0),  idx*20  ); 
+	osg::Vec3 coord( xoffset -200*sin(idx/10.0), -200*cos(idx/10.0),  idx*20*zscale  ); 
 	coords->push_back( coord );
 	primset2->push_back( ci++ );
     }
 
     osg::ref_ptr<osgGeo::PolyLineNode> polyline = new osgGeo::PolyLineNode();
-    polyline->setRadius( 50 );
-    polyline->setColor( osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f) );
+    polyline->setRadius( 20 );
+    osg::Material* mat = new osg::Material;
+    mat->setDiffuse( osg::Material::FRONT_AND_BACK, col );
+    polyline->getOrCreateStateSet()->setAttribute( mat );
     polyline->setVertexArray( coords );
     polyline->addPrimitiveSet( primset1 );
     polyline->addPrimitiveSet( primset2 );
@@ -62,9 +68,19 @@ osg::ref_ptr<osg::Node> drawDNA()
 
 int main()
 {
+    osg::Group* group = new osg::Group;
+    group->getOrCreateStateSet()->setMode(GL_NORMALIZE, osg::StateAttribute::ON); 
+    group->addChild( drawDNA( 200, 2, osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f))  );
+ 
+    osg::MatrixTransform* trans = new osg::MatrixTransform;
+    group->addChild( trans );
+    trans->setMatrix( osg::Matrix::scale( osg::Vec3d(1,1,2) ) );
+    trans->addChild( drawDNA( 0, 1, osg::Vec4(0.0f, 1.0f, 0.0f, 1.0f) ) );
+
     osgViewer::Viewer viewer;
-    viewer.setSceneData ( drawDNA() );
+    viewer.setSceneData ( group );
     viewer.setUpViewInWindow( 150, 20, 640, 480 );
+    viewer.setCameraManipulator( new osgGA::TrackballManipulator );
     viewer.run();
 }
 
