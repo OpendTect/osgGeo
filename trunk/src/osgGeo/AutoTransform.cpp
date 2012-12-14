3469beedbbb33e287b741707af30fa4213428151
jaap.glas@dgbes.com <jaap.glas@dgbes.com@1a978a74-06dc-1005-337c-a6ef347e0f4e>
2012-12-14 11:17:50 +0000

    Development of scale-resistant AutoTransform stalled, because of intended redesign of model-to-screen transformation chain.

--- trunk/src/osgGeo/AutoTransform.cpp
+++ trunk/src/osgGeo/AutoTransform.cpp
@@ -20,265 +20,113 @@ $Id: OneSideRender.cpp 108 2012-10-08 08:32:40Z kristofer.tingdahl@dgbes.com $
 #include "osgGeo/AutoTransform"
 
 #include <osg/CullStack>
+#include <cstdio>
+#include <iostream>
 
 
 namespace osgGeo
 {
 
 AutoTransform::AutoTransform()
-    :_restoreProportions(false)
+    :_restoreProportions(false),
+    _scaledBack(false),
+    _invMatrixDirty(true)
 {
 }
+
 
 AutoTransform::AutoTransform(const AutoTransform& pat,const osg::CopyOp& copyop)
     :osg::AutoTransform(pat,copyop),
-    _restoreProportions(pat._restoreProportions)
+    _restoreProportions(pat._restoreProportions),
+    _scaledBack(false),
+    _invMatrixDirty(true)
 {
 }
+
+
+bool AutoTransform::computeLocalToWorldMatrix(osg::Matrix& matrix,osg::NodeVisitor* nv) const
+{
+    if ( _scaledBack && _matrixDirty )
+    {
+	computeMatrix();
+
+	if ( !_restoreProportions )
+	     _cachedMatrix.preMult( osg::Matrix::scale(_modelViewScale) );
+
+	_invMatrixDirty = true;
+    }
+
+    if (_referenceFrame==RELATIVE_RF)
+	matrix.preMult(_cachedMatrix);
+    else // absolute
+	matrix = _cachedMatrix;
+
+    return true;
+}
+
+
+bool AutoTransform::computeWorldToLocalMatrix(osg::Matrix& matrix,osg::NodeVisitor* nv) const
+{
+    if ( _invMatrixDirty )
+    {
+	osg::Matrix mat;
+	mat.makeIdentity();
+	computeLocalToWorldMatrix( mat,  nv );
+	_cachedInvMatrix = osg::Matrix::inverse(mat);
+	_invMatrixDirty = false;
+    }
+
+    matrix.postMult( _cachedInvMatrix );
+    return true;
+}
+
 
 void AutoTransform::accept(osg::NodeVisitor& nv)
 {
-    if (nv.validNodeMask(*this))
+    if ( !nv.validNodeMask(*this) )
+	return;
+
+    if ( nv.getVisitorType()!=osg::NodeVisitor::CULL_VISITOR )
+	return;
+
+    osg::CullStack* cs = dynamic_cast<osg::CullStack*>(&nv);
+    osg::ref_ptr<osg::RefMatrix> mvm = new osg::RefMatrix(*cs->getModelViewMatrix());
+
+    if ( _firstTimeToInitEyePoint || _lastModelViewMatrix!=*mvm )
     {
-	// if app traversal update the frame count.
-	if (nv.getVisitorType()==osg::NodeVisitor::UPDATE_VISITOR)
-	{
-	}
-	else
-	    if (nv.getVisitorType()==osg::NodeVisitor::CULL_VISITOR)
-	    {
-
-		osg::CullStack* cs = dynamic_cast<osg::CullStack*>(&nv);
-		if (cs)
-		{
-
-		    osg::Viewport::value_type width = _previousWidth;
-		    osg::Viewport::value_type height = _previousHeight;
-
-		    osg::Viewport* viewport = cs->getViewport();
-		    if (viewport)
-		    {
-			width = viewport->width();
-			height = viewport->height();
-		    }
-
-		    osg::Vec3d eyePoint = cs->getEyeLocal();
-		    osg::Vec3d localUp = cs->getUpLocal();
-		    osg::Vec3d position = getPosition();
-
-		    const osg::Matrix& projection = *(cs->getProjectionMatrix());
-
-		    bool doUpdate = _firstTimeToInitEyePoint;
-		    if (!_firstTimeToInitEyePoint)
-		    {
-			osg::Vec3d dv = _previousEyePoint-eyePoint;
-			if (dv.length2()>getAutoUpdateEyeMovementTolerance()*(eyePoint-getPosition()).length2())
-			{
-			    doUpdate = true;
-			}
-			osg::Vec3d dupv = _previousLocalUp-localUp;
-			// rotating the camera only affects ROTATE_TO_*
-			if (_autoRotateMode &&
-			    dupv.length2()>getAutoUpdateEyeMovementTolerance())
-			{
-			    doUpdate = true;
-			}
-			else if (width!=_previousWidth || height!=_previousHeight)
-			{
-			    doUpdate = true;
-			}
-			else if (projection != _previousProjection)
-			{
-			    doUpdate = true;
-			}
-			else if (position != _previousPosition)
-			{
-			    doUpdate = true;
-			}
-		    }
-		    _firstTimeToInitEyePoint = false;
-
-		    if (doUpdate)
-		    {
-
-			if (getAutoScaleToScreen())
-			{
-			    const float pixelFactor(0.48f); // With a very low aspect ratio pixel size 
-							    // which can be as low a half of the true pixel size
-			    const double nativesize = 1.0/cs->pixelSize(getPosition(),pixelFactor);
-			    osg::Vec3d newscale( nativesize, nativesize, nativesize );
-			    if ( _restoreProportions )
-			    {
-				osg::RefMatrix* modelviewmatrix = cs->getModelViewMatrix();
-				if ( modelviewmatrix )
-				{
-				    osg::Vec3d translation;
-				    osg::Quat rotation;
-				    osg::Vec3d modelscale;
-				    osg::Quat so;
-
-				    modelviewmatrix->decompose( translation, rotation, modelscale, so );
-
-				    modelscale.normalize();
-
-				    newscale[0] = nativesize/modelscale[0];
-				    newscale[1] = nativesize/modelscale[1];
-				    newscale[2] = nativesize/modelscale[2];
-				}
-			    }
-
-			    double length = newscale.length();
-			    newscale.normalize();
-
-			    if (_autoScaleTransitionWidthRatio>0.0)
-			    {
-				if (_minimumScale>0.0)
-				{
-				    double j = _minimumScale;
-				    double i = (_maximumScale<DBL_MAX) ?
-					_minimumScale+(_maximumScale-_minimumScale)*_autoScaleTransitionWidthRatio :
-				    _minimumScale*(1.0+_autoScaleTransitionWidthRatio);
-				    double c = 1.0/(4.0*(i-j));
-				    double b = 1.0 - 2.0*c*i;
-				    double a = j + b*b / (4.0*c);
-				    double k = -b / (2.0*c);
-
-				    if (length<k) length = _minimumScale;
-				    else if (length<i) length = a + b*length + c*(length*length);
-				}
-
-				if (_maximumScale<DBL_MAX)
-				{
-				    double n = _maximumScale;
-				    double m = (_minimumScale>0.0) ?
-					_maximumScale+(_minimumScale-_maximumScale)*_autoScaleTransitionWidthRatio :
-				    _maximumScale*(1.0-_autoScaleTransitionWidthRatio);
-				    double c = 1.0 / (4.0*(m-n));
-				    double b = 1.0 - 2.0*c*m;
-				    double a = n + b*b/(4.0*c);
-				    double p = -b / (2.0*c);
-
-				    if (length>p) length = _maximumScale;
-				    else if (length>m) length = a + b*length + c*(length*length);
-				}
-			    }
-
-			    newscale *= length;
-			    setScale(newscale);
-			}
-
-			if (_autoRotateMode==ROTATE_TO_SCREEN)
-			{
-			    osg::Vec3d translation;
-			    osg::Quat rotation;
-			    osg::Vec3d scale;
-			    osg::Quat so;
-
-			    cs->getModelViewMatrix()->decompose( translation, rotation, scale, so );
-
-			    setRotation(rotation.inverse());
-			}
-			else if (_autoRotateMode==ROTATE_TO_CAMERA)
-			{
-			    osg::Vec3d PosToEye = _position - eyePoint;
-			    osg::Matrix lookto = osg::Matrix::lookAt(
-				osg::Vec3d(0,0,0), PosToEye, localUp);
-			    osg::Quat q;
-			    q.set(osg::Matrix::inverse(lookto));
-			    setRotation(q);
-			}
-			else if (_autoRotateMode==ROTATE_TO_AXIS)
-			{
-			    osg::Matrix matrix;
-			    osg::Vec3 ev(eyePoint - _position);
-
-			    switch(_cachedMode)
-			    {
-			    case(AXIAL_ROT_Z_AXIS):
-				{
-				    ev.z() = 0.0f;
-				    float ev_length = ev.length();
-				    if (ev_length>0.0f)
-				    {
-					//float rotation_zrotation_z = atan2f(ev.x(),ev.y());
-					//mat.makeRotate(inRadians(rotation_z),0.0f,0.0f,1.0f);
-					float inv = 1.0f/ev_length;
-					float s = ev.x()*inv;
-					float c = -ev.y()*inv;
-					matrix(0,0) = c;
-					matrix(1,0) = -s;
-					matrix(0,1) = s;
-					matrix(1,1) = c;
-				    }
-				    break;
-				}
-			    case(AXIAL_ROT_Y_AXIS):
-				{
-				    ev.y() = 0.0f;
-				    float ev_length = ev.length();
-				    if (ev_length>0.0f)
-				    {
-					//float rotation_zrotation_z = atan2f(ev.x(),ev.y());
-					//mat.makeRotate(inRadians(rotation_z),0.0f,0.0f,1.0f);
-					float inv = 1.0f/ev_length;
-					float s = -ev.z()*inv;
-					float c = ev.x()*inv;
-					matrix(0,0) = c;
-					matrix(2,0) = s;
-					matrix(0,2) = -s;
-					matrix(2,2) = c;
-				    }
-				    break;
-				}
-			    case(AXIAL_ROT_X_AXIS):
-				{
-				    ev.x() = 0.0f;
-				    float ev_length = ev.length();
-				    if (ev_length>0.0f)
-				    {
-					//float rotation_zrotation_z = atan2f(ev.x(),ev.y());
-					//mat.makeRotate(inRadians(rotation_z),0.0f,0.0f,1.0f);
-					float inv = 1.0f/ev_length;
-					float s = -ev.z()*inv;
-					float c = -ev.y()*inv;
-					matrix(1,1) = c;
-					matrix(2,1) = -s;
-					matrix(1,2) = s;
-					matrix(2,2) = c;
-				    }
-				    break;
-				}
-			    case(ROTATE_TO_AXIS): // need to implement
-				{
-				    float ev_side = ev*_side;
-				    float ev_normal = ev*_normal;
-				    float rotation = atan2f(ev_side,ev_normal);
-				    matrix.makeRotate(rotation,_axis);
-				    break;
-				}
-			    }
-			    osg::Quat q;
-			    q.set(matrix);
-			    setRotation(q);
-			}
-
-			_previousEyePoint = eyePoint;
-			_previousLocalUp = localUp;
-			_previousWidth = width;
-			_previousHeight = height;
-			_previousProjection = projection;
-			_previousPosition = position;
-
-			_matrixDirty = true;
-		    }
-
-		}
-	    }
-
-	    // now do the proper accept
-	    Transform::accept(nv);
+	osg::Vec3d trans;
+	osg::Quat rot, so;
+	mvm->decompose( trans, rot, _modelViewScale, so );
+	_lastModelViewMatrix = *mvm;
     }
+
+    osg::Vec3d invMVS( 1.0/_modelViewScale[0], 1.0/_modelViewScale[1], 1.0/_modelViewScale[2] );
+    mvm->preMult( osg::Matrix::scale(invMVS) );
+
+    cs->pushModelViewMatrix( mvm.get(), _referenceFrame );
+
+    const osg::Vec3d usrPos = _position;
+    const osg::Vec3d usrPivot = _pivotPoint;
+    const double usrMinScale = _minimumScale;
+    const double usrMaxScale = _maximumScale;
+
+    _position = osg::Matrix::scale(_modelViewScale).preMult( usrPos );
+    _pivotPoint = osg::Matrix::scale(_modelViewScale).preMult( usrPivot );
+    _minimumScale = 0.0;
+    _maximumScale = DBL_MAX;
+    _scaledBack = true;
+
+    osg::AutoTransform::accept( nv );
+
+    _scaledBack = false;
+    _position = usrPos;
+    _pivotPoint = usrPivot;
+    _minimumScale = usrMinScale;
+    _maximumScale = usrMaxScale;                                               
+                                    
+    cs->popModelViewMatrix();
 }
+
 
 } // end namespace
 
