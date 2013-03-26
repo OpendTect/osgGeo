60d9af0b656c007ade7f6ee053600ff1ad9ce64b
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2013-03-26 11:52:17 +0000

    Moved to internal cdash.

--- CTestConfig.cmake
+++ CTestConfig.cmake
@@ -12,6 +12,6 @@ set(CTEST_PROJECT_NAME "osgGeo")
 set(CTEST_NIGHTLY_START_TIME "01:00:00 GMT")
 
 set(CTEST_DROP_METHOD "http")
-set(CTEST_DROP_SITE "my.cdash.org")
+set(CTEST_DROP_SITE "intranet")
 set(CTEST_DROP_LOCATION "/submit.php?project=osgGeo")
 set(CTEST_DROP_SITE_CDASH TRUE)
