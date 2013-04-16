cee7329ab7c5182b70abb2b3fe660918dd62c037
Kristofer Tingdahl <kristofer.tingdahl@dgbes.com>
2013-04-16 12:34:42 +0000

    Made file compile on Linux/Mac
    Summary: This was found like this on CDash.
    
    Test Plan: Not tested (apart from that it compiles).
    
    Reviewers: ranojay
    
    Reviewed By: ranojay
    
    Differential Revision: https://phabricator.dgbes.com:8443/D458

--- src/osgGeo/PolygonSelection.cpp
+++ src/osgGeo/PolygonSelection.cpp
@@ -14,7 +14,7 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-$Id: PolygonSelection.cpp 169 2013-01-18 11:47:07Z ranojay.sen@dgbes.com $
+$Id$
 
 */
 
