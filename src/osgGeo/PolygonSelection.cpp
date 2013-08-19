d008020538935f196e3ba967ec4e1897200781f2
Ranojay Sen <ranojay.sen@dgbes.com>
2013-08-19 04:34:35 +0000

    Some improvements to osgGeo::PolygonSelection
    Summary:
    Notable changes include
    1) The class now has a copy of the master camera so createEventHandler(); function is made protected.
    2) Has implementation for projecting 3D coordinates to 2d screen.
    3) General improvements and code style changes.
    
    Test Plan: Working for the od_osg branch
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    CC: ding
    
    Differential Revision: https://phabricator.dgbes.com:8443/D1632

--- src/osgGeo/PolygonSelection.cpp
+++ src/osgGeo/PolygonSelection.cpp
@@ -21,6 +21,7 @@ $Id$
 
 #include <osgGeo/PolygonSelection>
 
+#include <osg/Camera>
 #include <osg/Geode>
 #include <osg/Geometry>
 #include <osg/Material>
@@ -37,17 +38,17 @@ public:
     { _polysel = ps; }
     
 protected:
-    bool handle ( const osgGA::GUIEventAdapter &ea,
-						osgGA::GUIActionAdapter&,
-						osg::Object*,
-						osg::NodeVisitor * )
+    bool handle (const osgGA::GUIEventAdapter &ea,
+					osgGA::GUIActionAdapter&,
+					osg::Object*,
+					osg::NodeVisitor*)
     {
-	return _polysel->handleEvent( ea );
+	return _polysel->handleEvent(ea);
     }
 
-    bool handle ( const osgGA::GUIEventAdapter &ea, osgGA::GUIActionAdapter& aa )
+    bool handle(const osgGA::GUIEventAdapter &ea,osgGA::GUIActionAdapter& aa)
     {
-	return osgGA::GUIEventHandler::handle( ea, aa );
+	return osgGA::GUIEventHandler::handle(ea,aa);
     }
 
     osg::ref_ptr<osgGeo::PolygonSelection>	    _polysel;
@@ -57,65 +58,130 @@ protected:
 PolygonSelection::PolygonSelection()
     : _geode(new osg::Geode)
     , _ison(true)
-    , _linegeometry(new osg::Geometry)
+    , _lineGeometry(new osg::Geometry)
     , _coords(new osg::Vec3Array) 
-    , _coordslist(new osg::DrawArrays(GL_LINE_LOOP))
-    , _shapetype(Rectangle)
+    , _coordsList(new osg::DrawArrays(GL_LINE_LOOP))
+    , _shapeType(Polygon)
     , _callback(0)
     , _material(0)
     , _zcoord(0)
+    , _masterCamera(0)
 {
-    _linegeometry->setVertexArray( _coords );
-    _linegeometry->addPrimitiveSet( _coordslist );
-    _geode->addDrawable( _linegeometry );
-    setColor( osg::Vec4(1,0,0,1) );
+    _lineGeometry->setVertexArray(_coords);
+    _lineGeometry->addPrimitiveSet(_coordsList);
+    _geode->addDrawable(_lineGeometry);
+    setColor(osg::Vec4(1,0,0,1));
 }
 
 
 PolygonSelection::~PolygonSelection()
 {
+    if ( _masterCamera )
+	_masterCamera->unref();
+}
+
+
+void PolygonSelection::addEventHandlerCamera( osg::Camera* camera )
+{
+    if ( !_masterCamera )
+    {
+	_masterCamera = camera;
+	_masterCamera->ref();
+	_masterCamera->addEventCallback( createEventHandler() );
+    }
 }
 
 
 osgGA::GUIEventHandler* PolygonSelection::createEventHandler()
 {
     PolygonSelectionEventHandler* res = new PolygonSelectionEventHandler;
-    res->setPolygonSelector( this );
+    res->setPolygonSelector(this);
     return res;
 }
 
 
-void PolygonSelection::setLatestMousePoints( const osg::Vec3& pos )
-{   
-    if ( _shapetype == Rectangle )
-    {
-	(*_coords)[1] = osg::Vec3( pos.x(), _coords->at(0).y(), pos.z() );
-	(*_coords)[2] = pos; 
-	(*_coords)[3] = osg::Vec3( _coords->at(0).x(), pos.y(), pos.z() );
-	_coordslist->setCount( _coords->size() );
-    }
-    else
-    {
-	_coords->push_back( pos );
-	_coordslist->setCount( _coords->size() );
-    }
-
-    _linegeometry->dirtyDisplayList();
-    dirtyBound();
-}
-
-
-void PolygonSelection::accept( osg::NodeVisitor& nv )
+void PolygonSelection::accept(osg::NodeVisitor& nv)
 {
     osg::ref_ptr<osg::StateSet> stateset = getStateSet();
     osgUtil::CullVisitor* cv = dynamic_cast<osgUtil::CullVisitor*>(&nv);
-    if ( stateset && cv )
-	cv->pushStateSet( stateset );
+    if (stateset && cv)
+	cv->pushStateSet(stateset);
 
-    _geode->accept( nv );
+    _geode->accept(nv);
 
-    if ( stateset && cv )
+    if (stateset && cv)
 	cv->popStateSet();
+}
+
+
+bool PolygonSelection::handleEvent(const osgGA::GUIEventAdapter& ea)
+{
+    if (!_ison || _shapeType==Off)
+	return false;
+    
+    const osg::Vec3 mousepos(ea.getX(),ea.getY(),_zcoord);
+    if (ea.getEventType()==osgGA::GUIEventAdapter::PUSH 
+	&& ea.getButton()==osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON)
+    {
+	_coords->clear();
+	if ( _shapeType == Rectangle )
+	{
+	    _coords->resize(4);
+	    (*_coords)[0] = mousepos;
+	}
+	return true;
+    }
+    if (ea.getEventType()==osgGA::GUIEventAdapter::DRAG
+	&& ea.getButtonMask()==osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON)
+    {
+	setLatestMousePoints (mousepos);
+	return true;
+    }
+    if (ea.getEventType()==osgGA::GUIEventAdapter::RELEASE 
+	&& ea.getButton()==osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON)
+    {
+	if (_callback)
+	{
+	    osg::NodeVisitor nv;
+	    ( *_callback )(this,&nv);
+	}
+	return true;
+    }
+   
+    return false;
+}
+
+
+void PolygonSelection::setLatestMousePoints(const osg::Vec3& pos)
+{   
+    if (_shapeType == Rectangle)
+    {
+	(*_coords)[1] = osg::Vec3(pos.x(),_coords->at(0).y(),pos.z());
+	(*_coords)[2] = pos; 
+	(*_coords)[3] = osg::Vec3(_coords->at(0).x(),pos.y(),pos.z());
+	_coordsList->setCount(_coords->size());
+    }
+    else
+    {
+	_coords->push_back(pos);
+	_coordsList->setCount(_coords->size());
+    }
+
+    _lineGeometry->dirtyDisplayList();
+}
+
+
+osg::Vec2 PolygonSelection::projectPointToScreen(osg::Vec3 pointin3d) const
+{
+    if ( !_masterCamera )
+	return osg::Vec2(1e30, 1e30);
+
+     const osg::Matrix vpwmatrix(_masterCamera->getViewMatrix() *
+			   _masterCamera->getProjectionMatrix() *
+			   _masterCamera->getViewport()->computeWindowMatrix());
+     const osg::Vec3 pointin2d = pointin3d * vpwmatrix;
+     const osg::Vec2 point2d(pointin2d.x(), pointin2d.y());
+     return point2d;
 }
 
 
@@ -125,83 +191,53 @@ osg::BoundingSphere PolygonSelection::computeBound() const
 }
 
 
-void PolygonSelection::setColor( const osg::Vec4& color )
+void PolygonSelection::clear()
+{
+    _coords->clear();
+    _coordsList->setCount(0);
+    _lineGeometry->dirtyDisplayList();
+}
+
+
+void PolygonSelection::setColor(const osg::Vec4& color)
 {
    osg::StateSet* stateset = getOrCreateStateSet();
    stateset->setMode(GL_LIGHTING, osg::StateAttribute::OFF);
    if ( !_material )
        _material = new osg::Material;
-   _material->setDiffuse( osg::Material::FRONT, color );
-   stateset->setAttribute( _material );
+   _material->setDiffuse(osg::Material::FRONT, color);
+   stateset->setAttribute(_material);
 }
 
 
-bool PolygonSelection::handleEvent( const osgGA::GUIEventAdapter& ea )
-				                   
+void PolygonSelection::setShapeType(ShapeType st)
 {
-    if ( !_ison )
-	return false;
-    
-    const osg::Vec3 mousepos( ea.getX(), ea.getY(), _zcoord );
-    if ( ea.getEventType()==osgGA::GUIEventAdapter::PUSH 
-	&& ea.getButton()==osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON  )
-    {
-	_coords->clear();
-	if ( _shapetype == Rectangle )
-	{
-	    _coords->resize( 4 );
-	    (*_coords)[0] = mousepos;
-	}
-	return true;
-    }
-    if ( ea.getEventType()==osgGA::GUIEventAdapter::DRAG
-	&& ea.getButtonMask()==osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON )
-    {
-	setLatestMousePoints ( mousepos );
-	return true;
-    }
-    if ( ea.getEventType()==osgGA::GUIEventAdapter::RELEASE 
-	&& ea.getButton()==osgGA::GUIEventAdapter::LEFT_MOUSE_BUTTON )
-    {
-	if ( _callback )
-	{
-	    osg::NodeVisitor nv;
-	    ( *_callback )( this, &nv );
-	}
-	return true;
-    }
-   
-    return false;
+    _shapeType = st;
+    dirtyBound();
 }
 
 
-void PolygonSelection::setShapeType( ShapeType st )
-{
-    _shapetype = st;
-}
-
-
-void PolygonSelection::setZCoord( float zcoord )
+void PolygonSelection::setZCoord(float zcoord)
 {
     _zcoord = zcoord;
 }
 
 
-void PolygonSelection::addCallBack( osg::NodeCallback *nc )
+void PolygonSelection::addCallBack(osg::NodeCallback *nc)
 {
-    if ( !_callback )
+    if (!_callback)
 	_callback = nc;
     else
-	_callback->addNestedCallback( nc );
+	_callback->addNestedCallback(nc);
 }
 
 
-void PolygonSelection::removeCallBack( osg::NodeCallback *nc )
+void PolygonSelection::removeCallBack(osg::NodeCallback *nc)
 {
     if ( nc == _callback )
 	_callback = _callback->getNestedCallback();
     else
-	_callback->removeNestedCallback( nc );
+	_callback->removeNestedCallback(nc);
 }
 
 } // osgGeo
