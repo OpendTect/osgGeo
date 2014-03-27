596786282961bf03d29de7fa36570f5b7f6ae5ab
Ding Zheng <ding.zheng@dgbes.com>
2014-03-27 13:42:45 +0000

    Osg View Heigh Resolution Screen Shot
    Summary:
    1. Class ViewPoster is the interface to generate heigh resolution view image.
    2. PrinterPoster file is a copy from osg example osgPoster. I just did a slight modification in getting the final image.
    3. Since the relation between heigh resolution and original resolution is multiple times relation in row and column of view image, once
       the user want to create the image poster in any size such as in the "setting option" in the od UI dialog, the we have to change the original view 
       size (viewport) to meet above relation. thus the view scene will be always in the center of poster. after done posting, we will change the viewport back.
       ( I am not sure how does coin handle with it, but from the output, it seems coin does the same thing)
    4. Although the 3 works fine. right now we can keep original user interface (dialog) to general any size of image, but it is painful when we change the 
       viewport (if we use screen option and lock the respect ratio, without this problem), the original view is moved and then moved back after done posting.
       This problem needs to be solved. 
       So, if changing viewport, let it accept renderer but don't be displayed by this viewport.
    
    Test Plan: Compiles and Tests fine
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Maniphest Tasks: T158
    
    Differential Revision: https://phabricator.dgbes.com:8443/D3287

---
+++ src/osgGeo/TiledOffScreenRenderer.cpp
@@ -0,0 +1,281 @@
+/* osgGeo - A collection of geoscientific extensions to OpenSceneGraph.
+Copyright 2011- dGB Beheer B.V.
+
+osgGeo is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+$Id: TiledOffScreenRenderer.cpp 346 2014-02-07 09:31:17Z ding.zheng@dgbes.com $
+
+*/
+#include <osgGeo/TiledOffScreenRenderer>
+
+#include <osg/Viewport>
+#include <osg/Switch>
+#include <osgDB/WriteFile>
+#include <osgViewer/View>
+#include <osgViewer/Viewer>
+#include <osgViewer/CompositeViewer>
+
+using namespace osgGeo;
+
+TiledOffScreenRenderer::TiledOffScreenRenderer(osgViewer::View* view,
+					       osgViewer::CompositeViewer* viewer)
+    :_view(view)
+    ,_viewer(viewer)
+    ,_tileWidth(640)
+    ,_tileHeight(480)
+    ,_width(0)
+    ,_height(0)
+    ,_collectorCamera(0)
+    ,_orientationCamera(view->getCamera())
+    ,_cameraSwitch(dynamic_cast<osg::Switch*>(view->getSceneData()))
+{
+    if (!_cameraSwitch)
+    {
+	_cameraSwitch = new osg::Switch;
+	_cameraSwitch->addChild(view->getSceneData());
+	view->setSceneData(_cameraSwitch);
+    }
+}
+
+
+TiledOffScreenRenderer::~TiledOffScreenRenderer()
+{
+    if ( _cameraSwitch->getNumChildren()<= 0)
+	return;
+    _cameraSwitch->removeChild(_collectorCamera);
+    _cameraSwitch->setValue(0, true);
+}
+
+
+void TiledOffScreenRenderer::setOutputSize(int width, int length)
+{
+    _width = width;
+    _height = length;
+
+    const osg::Viewport* viewport = _orientationCamera->getViewport();
+
+    const int widthTimes = ceil(_width/viewport->width());
+    const int heighTimes = ceil(_height/viewport->height());
+
+    _tileWidth  = (int)(_width/widthTimes);
+    _tileHeight = (int)(_height/heighTimes);
+}
+
+
+bool TiledOffScreenRenderer::createOutput()
+{
+    createImageCollectorCamera();
+    setupImageCollector();
+
+    return doRender();
+}
+
+
+void TiledOffScreenRenderer::createImageCollectorCamera()
+{
+    _collectorCamera = new osg::Camera;
+    _collectorCamera->setClearColor(_orientationCamera->getClearColor());
+    _collectorCamera->setClearMask(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+    _collectorCamera->setReferenceFrame(osg::Transform::ABSOLUTE_RF);
+    _collectorCamera->setRenderOrder(osg::Camera::PRE_RENDER);
+    _collectorCamera->setRenderTargetImplementation(osg::Camera::FRAME_BUFFER);
+    osg::ref_ptr<osg::Viewport> viewport = new osg::Viewport(0,0,_tileWidth,_tileHeight);
+    _collectorCamera->setViewport(viewport);
+
+     osg::Node* scenedata = _cameraSwitch->getChild(0);
+    _collectorCamera->addChild(scenedata);
+    _cameraSwitch->setValue(0, false);
+    _cameraSwitch->addChild(_collectorCamera);
+    _cameraSwitch->setValue(_cameraSwitch->getChildIndex(_collectorCamera), true);
+}
+
+
+void TiledOffScreenRenderer::setupImageCollector()
+{
+    _imageCollector = new OffscreenTileImageCollector;
+    _imageCollector->setTileSize(_tileWidth, _tileHeight);
+    _imageCollector->setFinalImageSize(_width, _height);
+    _imageCollector->setCaptureCamera(_collectorCamera);
+
+    osg::ref_ptr<osg::Image> finalImage = new osg::Image;
+    finalImage->allocateImage(_width, _height , 1, GL_RGBA, GL_UNSIGNED_BYTE);
+    _imageCollector->setFinalImage(finalImage.get());
+}
+
+
+bool TiledOffScreenRenderer::doRender()
+{
+    _imageCollector->setCameraOrientation(_orientationCamera->getViewMatrix(),
+					 _orientationCamera->getProjectionMatrix());
+
+    const osgViewer::ViewerBase::FrameScheme oldscheme = _viewer->getRunFrameScheme();
+    _viewer->setRunFrameScheme(osgViewer::ViewerBase::CONTINUOUS);
+    while ( !_imageCollector->done() )
+    {
+	_viewer->advance();
+
+	_imageCollector->frame(_viewer->getFrameStamp(), _view->getSceneData());
+	_viewer->renderingTraversals();
+    }
+
+    _collectorCamera->setRenderingCache(NULL);
+    _collectorCamera->detach(osg::Camera::COLOR_BUFFER);
+
+    _viewer->setRunFrameScheme(oldscheme);
+
+    if ( !_imageCollector->getFinalImage() )
+	return false;
+
+    return true;
+}
+
+
+void TiledOffScreenRenderer::writeOutputFile(const std::string& fileName)
+{
+  if( !_imageCollector ) 
+	return;
+
+    osg::ref_ptr<osg::Image> outputImage = _imageCollector->getFinalImage();
+    if ( outputImage )
+	osgDB::writeImageFile(*outputImage,fileName);
+}
+
+
+const osg::Image* TiledOffScreenRenderer::getOutput() const
+{
+    if( !_imageCollector  ) 
+	return 0;
+
+    return _imageCollector->getFinalImage();
+}
+
+
+TiledOffScreenRenderer::OffscreenTileImageCollector::OffscreenTileImageCollector()
+    :_isRunning(true)
+    ,_isFinishing(false)
+    ,_lastBindingFrame(0)
+    ,_currentRow(0)
+    ,_currentColumn(0)
+    ,_camera(0)
+    ,_finalImage(0)
+{
+}
+
+void TiledOffScreenRenderer::OffscreenTileImageCollector::setCameraOrientation(
+    const osg::Matrixd& view, const osg::Matrixd& proj)
+{
+    _currentViewMatrix = view;
+    _currentProjectionMatrix = proj;
+}
+
+
+void TiledOffScreenRenderer::OffscreenTileImageCollector::frame(
+    const osg::FrameStamp* fs, osg::Node* node)
+{
+    if ( _isFinishing )
+    {
+	if ( (fs->getFrameNumber()-_lastBindingFrame)>2 )
+	{
+	    recordImages();
+	    _isFinishing = false;
+	}
+    }
+
+    const int tileRows = (int)(_finalImageSize.y() / _tileSize.y());
+    const int tileColumns = (int)(_finalImageSize.x() / _tileSize.x());
+
+    if ( _isRunning )
+    {
+	// Every "copy-to-image" process seems to be finished in 2 frames.
+	// So record them and dispatch camera to next tiles.
+	if ( (fs->getFrameNumber()-_lastBindingFrame)>2 )
+	{
+	    // Record images and unref them to free memory
+	    recordImages();
+	    if ( _camera.valid() )
+	    {
+		bindCameraToImage(_camera.get(), _currentRow, _currentColumn);
+		if ( _currentColumn<tileColumns-1 )
+		{
+		    _currentColumn++;
+		}
+		else
+		{
+		    if ( _currentRow<tileRows-1 )
+		    {
+			_currentRow++;
+			_currentColumn = 0;
+		    }
+		    else
+		    {
+			_isRunning = false;
+			_isFinishing = true;
+		    }
+		}
+	    }
+	    _lastBindingFrame = fs->getFrameNumber();
+	}
+    }
+}
+
+
+void TiledOffScreenRenderer::OffscreenTileImageCollector::bindCameraToImage(
+    osg::Camera* camera, int row, int col )
+{
+    std::stringstream stream;
+    stream << "image_" << row << "_" << col;
+
+    osg::ref_ptr<osg::Image> image = new osg::Image;
+    image->setName(stream.str());
+    image->allocateImage((int)_tileSize.x(), (int)_tileSize.y(), 1, GL_RGBA, GL_UNSIGNED_BYTE);
+    _images[TilePosition(row,col)] = image.get();
+
+    const int tileRows = (int)(_finalImageSize.y() / _tileSize.y());
+    const int tileColumns = (int)(_finalImageSize.x() / _tileSize.x());
+    // Calculate projection matrix offset of each tile
+    osg::Matrix offsetMatrix =
+	osg::Matrix::scale(tileColumns, tileRows, 1.0) *
+	osg::Matrix::translate(tileColumns-1-2*col, tileRows-1-2*row, 0.0);
+    camera->setViewMatrix(_currentViewMatrix);
+    camera->setProjectionMatrix(_currentProjectionMatrix * offsetMatrix);
+
+    // Reattach cameras and new allocated images
+    camera->setRenderingCache( NULL );  
+    camera->detach(osg::Camera::COLOR_BUFFER);
+    camera->attach(osg::Camera::COLOR_BUFFER, image.get(), 0, 0);
+}
+
+
+void TiledOffScreenRenderer::OffscreenTileImageCollector::recordImages()
+{
+    for ( TileImages::iterator itr=_images.begin(); itr!=_images.end(); ++itr )
+    {
+	osg::Image* image = (itr->second).get();
+	if ( _finalImage.valid() )
+	{
+	    // FIXME: A stupid way to combine tile images to final result. Any better ideas?
+	    unsigned int row = itr->first.first, col = itr->first.second;
+	    for ( int t=0; t<image->t(); ++t )
+	    {
+		unsigned char* source = image->data(0, t);
+		unsigned char* target = _finalImage->data( 
+		    col*(int)_tileSize.x(), t + row*(int)_tileSize.y());
+		memcpy(target, source, image->s() * 4 * sizeof(unsigned char));
+	    }
+	}
+
+    }
+
+    _images.clear();
+}
