5de89aed331e934ca3a3a845b93261ac1de1b8fc
Jaap Glas <jaap.glas@dgbes.com>
2015-05-07 10:38:14 +0200

    Bugfix: Clicking in scene results in huge delays if it contains hundreds of draggers. (PORTING FROM od TO od 5.0 PRESERVING BINARY COMPATIBILITY)
    Summary:
    Bugfix: Clicking in scene results in huge delays if it contains hundreds of
    draggers. It turns out that every OSG dragger intersects the camera ray with
    all scene objects to get an intersection list (osgManipulator/Dragger.cpp:371).
    view->computeIntersections(ea ,intersections,_intersectionMask). I don't see
    why this list cannot be shared or constructed only if the OSG dragger is under
    the mouse pointer itself. My solution overloads the traverse() function to
    test the latter condition in advance. We may ask Robert Osfield whether the
    current way of implementing this is a necessity or a bug. However, I guess
    he did not do the original implementation of the draggers, and the rest of
    the community does not use that many draggers in one scene.
    
    Test Plan:
    Tested in OpendTect using F3_Demo. I used Fault_A with every knot duplicated
    X times (by a code hack) and I used "Example random contour demo 1 ->MFS4"
    as it is. Both add hundreds of draggers to the scene. Varying X proves that
    the original order of computation is quadratic with the number of draggers.
    The bugfix reduces this to a linear order. No huge delays anymore when
    clicking in the scene, while clicking on a specific dragger to drag it
    does still work.
    
    Reviewers: kristofer
    
    Reviewed By: kristofer
    
    Differential Revision: https://phabricator.dgbes.com:8443/D7702

--- src/osgGeo/Draggers.cpp
+++ src/osgGeo/Draggers.cpp
@@ -18,6 +18,8 @@ $Id: TrackballManipulator.cpp 231 2013-04-16 12:35:57Z kristofer.tingdahl@dgbes.
 */
 
 #include <osgGeo/Draggers>
+#include <osgViewer/View>
+#include <osg/Version>
 
 
 namespace osgGeo
@@ -98,6 +100,148 @@ unsigned int Translate2DDragger::getInactivationModKeyMask() const
 }
 
 
+//===== TEMPORARY CLASSES TO PRESERVE BINARY COMPATIBILITY ====================
+
+
+FastTranslate1DDragger::FastTranslate1DDragger()
+    : osgManipulator::Translate1DDragger()
+    , _inactivationModKeyMask( 0 )
+{}
+
+
+FastTranslate1DDragger::FastTranslate1DDragger(const osg::Vec3d& s, const osg::Vec3d& e)
+    : osgManipulator::Translate1DDragger( s,e )
+    , _inactivationModKeyMask( 0 )
+{}
+
+
+void FastTranslate1DDragger::traverse(osg::NodeVisitor& nv)
+{
+    if (_handleEvents && nv.getVisitorType()==osg::NodeVisitor::EVENT_VISITOR)
+    {
+	osgGA::EventVisitor* ev = dynamic_cast<osgGA::EventVisitor*>(&nv);
+	if (ev)
+	{
+	    for(osgGA::EventQueue::Events::iterator itr = ev->getEvents().begin(); itr != ev->getEvents().end(); ++itr)
+	    {
+#if OSG_MIN_VERSION_REQUIRED(3,3,0)
+		osgGA::GUIEventAdapter* ea = itr->asGUIEventAdapter();
+#else
+		osgGA::GUIEventAdapter* ea = itr->get();
+#endif
+		// begin modification
+		if ( ea && ea->getEventType()==osgGA::GUIEventAdapter::PUSH )
+		{
+		    osgViewer::View* view = dynamic_cast<osgViewer::View*>(ev->getActionAdapter());
+		    if ( view )
+		    {
+			osgUtil::LineSegmentIntersector::Intersections intersections;
+			if ( !view->computeIntersections(*ea,nv.getNodePath(),intersections,_intersectionMask) ) continue;
+		    }
+		}
+		// end modification
+		if (Dragger::handle(*ea, *(ev->getActionAdapter()))) ea->setHandled(true);
+	    }
+	}
+    }
+    MatrixTransform::traverse(nv);
+}
+
+
+bool FastTranslate1DDragger::handle(const osgManipulator::PointerInfo& pointer, const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa)
+{
+    if ( ea.getEventType()==osgGA::GUIEventAdapter::PUSH )
+    {
+	if ( ea.getModKeyMask() & _inactivationModKeyMask )
+	    return false;
+    }
+
+    return osgManipulator::Translate1DDragger::handle( pointer, ea, aa );
+}
+
+
+void FastTranslate1DDragger::setInactivationModKeyMask(unsigned int mask)
+{
+    _inactivationModKeyMask = mask;
+}
+
+
+unsigned int FastTranslate1DDragger::getInactivationModKeyMask() const
+{
+    return _inactivationModKeyMask;
+}
+
+
+//=============================================================================
+
+
+FastTranslate2DDragger::FastTranslate2DDragger()
+    : osgManipulator::Translate2DDragger()
+    , _inactivationModKeyMask( 0 )
+{}
+
+
+FastTranslate2DDragger::FastTranslate2DDragger(const osg::Plane& plane)
+    : osgManipulator::Translate2DDragger( plane )
+    , _inactivationModKeyMask( 0 )
+{}
+
+
+void FastTranslate2DDragger::traverse(osg::NodeVisitor& nv)
+{
+    if (_handleEvents && nv.getVisitorType()==osg::NodeVisitor::EVENT_VISITOR)
+    {
+	osgGA::EventVisitor* ev = dynamic_cast<osgGA::EventVisitor*>(&nv);
+	if (ev)
+	{
+	    for(osgGA::EventQueue::Events::iterator itr = ev->getEvents().begin(); itr != ev->getEvents().end(); ++itr)
+	    {
+#if OSG_MIN_VERSION_REQUIRED(3,3,0)
+		osgGA::GUIEventAdapter* ea = itr->asGUIEventAdapter();
+#else
+		osgGA::GUIEventAdapter* ea = itr->get();
+#endif
+		// begin modification
+		if ( ea && ea->getEventType()==osgGA::GUIEventAdapter::PUSH )
+		{
+		    osgViewer::View* view = dynamic_cast<osgViewer::View*>(ev->getActionAdapter());
+		    if ( view )
+		    {
+			osgUtil::LineSegmentIntersector::Intersections intersections;
+			if ( !view->computeIntersections(*ea,nv.getNodePath(),intersections,_intersectionMask) ) continue;
+		    }
+		}
+		// end modification
+		if (Dragger::handle(*ea, *(ev->getActionAdapter()))) ea->setHandled(true);
+	    }
+	}
+    }
+    MatrixTransform::traverse(nv);
+}
+
+
+bool FastTranslate2DDragger::handle(const osgManipulator::PointerInfo& pointer, const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa)
+{
+    if ( ea.getEventType()==osgGA::GUIEventAdapter::PUSH )
+    {
+	if ( ea.getModKeyMask() & _inactivationModKeyMask )
+	    return false;
+    }
+
+    return osgManipulator::Translate2DDragger::handle( pointer, ea, aa );
+}
+
+
+void FastTranslate2DDragger::setInactivationModKeyMask(unsigned int mask)
+{
+    _inactivationModKeyMask = mask;
+}
+
+
+unsigned int FastTranslate2DDragger::getInactivationModKeyMask() const
+{
+    return _inactivationModKeyMask;
+}
 
 
 } // end namespace
