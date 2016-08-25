6ad600a2f7ea292bf6e8584f1e00012a2a995789
Jaap Glas <jaap.glas@dgbes.com>
2016-08-25 15:52:59 +0200

    [osgGeo1.2] Add isPlatformSupported() function and zero-pointer checks (OD-743)
    Summary:
    (1) Added isPlatformSupported() function to enable OpendTect to distinguish
    between "not yet implemented" and "unable to get GL info". The Windows
    implementation needs to be done by a developer on Windows. The code required
    should be extracted from spec/visBase/w32glxinfo.c alike Nanne did from
    spec/visBase/od_glxinfo for Linux.
    (2) One cannot assign a zero-pointer to a std::string. Since OSG crashed on
    glGetString(GL_VERSION) being zero in the case of Nouveau driver problems,
    we have to assure not to introduce the same problem here. I have no clue
    whether the glXMakeCurrent(dpy,win,ctx) condition will guard against such
    a crash in all circumstances.
    (3) Added glxinfo copyright header, since substantial portions of its code
    have been used over here.
    
    Test Plan:
    (1) Tested in OpendTect on Linux only.
    (2) Tested by temporarily replacing glGetString(GL_VERSION) by NULL.
    
    Reviewers: nanne
    
    Reviewed By: nanne
    
    Differential Revision: https://phabricator.dgbes.com:8443/D10751

---
+++ src/osgGeo/GLInfo.cpp
@@ -0,0 +1,152 @@
+/* osgGeo - A collection of geoscientific extensions to OpenSceneGraph.
+Copyright 2012 dGB Beheer B.V.
+
+osgGeo is free software; you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation; either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>
+
+$Id$
+
+*/
+
+
+/* This implementation of class osgGeo::GLInfo is using code from: 
+ *
+ * [1] wglinfo.c by Nate Robins, 1997 (for Windows)
+ *
+ * [2] glxinfo by Brian Paul, 1999-2006 (for Linux)
+ *
+ * Copyright (C) 1999-2006  Brian Paul   All Rights Reserved.
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice shall be included
+ * in all copies or substantial portions of the Software.
+ */
+
+
+#if defined(__win64__) || defined(__win32__)
+# include <windows.h>
+#else
+# include <GL/gl.h>
+# include <GL/glx.h>
+#endif
+
+#include <osgGeo/GLInfo>
+
+namespace osgGeo
+{
+
+GLInfo::GLInfo()
+{}
+
+
+bool GLInfo::isPlatformSupported() const
+{
+#if defined(__win64__) || defined(__win32__)
+    return false;
+#else
+    return true;
+#endif
+}
+
+
+bool GLInfo::get()
+{
+#if defined(__win64__) || defined(__win32__)
+    // TODO
+#else
+    Display* dpy = XOpenDisplay( NULL );
+    int attribSingle[] = {
+	GLX_RGBA,
+	GLX_RED_SIZE, 1,
+	GLX_GREEN_SIZE, 1,
+	GLX_BLUE_SIZE, 1,
+	None };
+    int attribDouble[] = {
+	GLX_RGBA,
+	GLX_RED_SIZE, 1,
+	GLX_GREEN_SIZE, 1,
+	GLX_BLUE_SIZE, 1,
+	GLX_DOUBLEBUFFER,
+	None };
+
+    int width = 100, height = 100;
+    int scrnum = 0;
+    Window root = RootWindow( dpy, scrnum );
+
+    XVisualInfo* visinfo = glXChooseVisual( dpy, scrnum, attribSingle );
+    if ( !visinfo )
+    {
+	visinfo = glXChooseVisual(dpy, scrnum, attribDouble);
+	if ( !visinfo )
+	{
+	    _errmsg = "Error: couldn't find RGB GLX visual";
+	    return false;
+	}
+    }
+
+    XSetWindowAttributes attr;
+    attr.background_pixel = 0;
+    attr.border_pixel = 0;
+    attr.colormap = XCreateColormap( dpy, root, visinfo->visual, AllocNone );
+    attr.event_mask = StructureNotifyMask | ExposureMask;
+
+    unsigned long mask = CWBackPixel | CWBorderPixel | CWColormap | CWEventMask;
+    Window win = XCreateWindow( dpy, root, 0, 0, width, height,
+				0, visinfo->depth, InputOutput,
+				visinfo->visual, mask, &attr );
+
+    Bool allowDirect = True;
+    GLXContext ctx = glXCreateContext( dpy, visinfo, NULL, allowDirect );
+    if ( !ctx )
+    {
+	_errmsg = "Error: glXCreateContext failed";
+	XFree(visinfo);
+	XDestroyWindow(dpy, win);
+	return false;
+    }
+
+    if ( glXMakeCurrent(dpy,win,ctx) )
+    {
+	if ( (const char*) glGetString(GL_VENDOR) )
+	    _glvendor = (const char*) glGetString(GL_VENDOR);
+	if ( (const char*) glGetString(GL_RENDERER) )
+	    _glrenderer = (const char*) glGetString(GL_RENDERER);
+	if ( (const char*) (const char*) glGetString(GL_VERSION) )
+	    _glversion = (const char*) glGetString(GL_VERSION);
+    }
+
+    glXDestroyContext(dpy, ctx);
+    XFree(visinfo);
+    XDestroyWindow(dpy, win);
+    XCloseDisplay(dpy);
+#endif
+    return true;
+}
+
+
+const char* GLInfo::glVendor() const
+{ return _glvendor.c_str(); }
+
+const char* GLInfo::glRenderer() const
+{ return _glrenderer.c_str(); }
+
+const char* GLInfo::glVersion() const
+{ return _glversion.c_str(); }
+
+} // namespace osgGeo
