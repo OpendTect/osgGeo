51a111f8c0b91e6630401215b0312fb69c8e0266
Jaap Glas <jaap.glas@dgbes.com>
2017-02-14 09:33:14 +0100

    [osgGeo-trunk] osg::Image::setColor(.) does not compile with OSG3.2 (Ding)
    Summary:
    osg::Image::setColor(.) does not compile with OSG3.2 (Ding). Added alternative
    code for lower OSG versions. Copied that code from LayeredTexture.ccp, where
    it worked fine for years.
    
    Test Plan:
    Use scene with inline plane and 2D line. Add preloaded default attributes
    with limited range subselection. Next run PDF3D and mail result to Android
    smartphone. Tested both branches of the added #if macro, and in both cases
    3D PDF reader shows the same green-gray undefined border color around the
    preloaded data on inline and 2D line.
    
    Reviewers: ding
    
    Reviewed By: ding
    
    Differential Revision: https://phabricator.dgbes.com:8443/D11441

--- src/osgGeo/TexturePanelStrip.cpp
+++ src/osgGeo/TexturePanelStrip.cpp
@@ -919,11 +919,26 @@ const osg::Image* TexturePanelStripNode::getCompositeTextureImage( bool addBorde
     const int id = _texture->compositeLayerId();
     const osg::Vec4f& borderColor = _texture->getDataLayerBorderColor( id );
 
+#if OSG_MIN_VERSION_REQUIRED(3,3,0)
     for ( int s=0; s<sSize; s++ )
     {
 	for ( int t=0; t<tSize; t++ )
 	    _compositeImageWithBorder->setColor( borderColor, s, t, 0 );
     }
+#else
+    // Assuming contiguous data of type GL_RGBA
+    unsigned char* imagePtr = _compositeImageWithBorder->data();
+    for ( int count=0; count<sSize*tSize; count++ )
+    {
+	for ( int channel=0; channel<=3; channel++ )
+	{
+	    int val = (int) floor( 255.0f*borderColor[channel] + 0.5 );
+	    val = val<=0 ? 0 : (val>=255 ? 255 : val);
+	    *imagePtr = (unsigned char) val;
+	    imagePtr++;
+	}
+    }
+#endif
 
     _compositeImageWithBorder->copySubImage( sBorder0, tBorder0, 0, compositeImage );
 
