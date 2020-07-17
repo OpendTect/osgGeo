b3ca0dc501d1ef2b47543d40defa7a4b37505613
Nageswara Rao <nageswara.rao@dgbes.com>
2020-07-17 12:19:14 +0530

    Hack to supress compilation warnings on windows platform compiling with VC2019.

--- src/osgGeo/LayeredTexture.cpp
+++ src/osgGeo/LayeredTexture.cpp
@@ -2574,7 +2574,10 @@ void LayeredTexture::setRenderingHint( bool stackIsOpaque )
 
 int LayeredTexture::getProcessInfo( std::vector<int>& layerIDs, int& nrUsedLayers, bool& useShaders, bool* stackIsOpaque ) const
 {
-    layerIDs.empty();
+    const bool isempty = layerIDs.empty();
+    if ( isempty )  //Hack to supress compilation warnings.
+	layerIDs.clear();
+
     std::vector<int> skippedIDs;
     int nrProc = 0;
     nrUsedLayers = -1;
