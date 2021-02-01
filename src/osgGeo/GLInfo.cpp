5e72170a940191892ac4b82cd681326da9b92feb
Arnaud Huck <arnaud.huck@dgbes.com>
2021-02-01 16:37:27 +0100

    Fixed a typo, causing wrong initialization on Windows [devel]

--- src/osgGeo/GLInfo.cpp
+++ src/osgGeo/GLInfo.cpp
@@ -19,21 +19,21 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>
 */
 
 
-/* This implementation of class osgGeo::GLInfo is using code from: 
+/* This implementation of class osgGeo::GLInfo is using code from:
  *
  * [1] wglinfo.c by Nate Robins, 1997 (for Windows)
  *
  * [2] glxinfo by Brian Paul, 1999-2006 (for Linux)
  *
  * Copyright (C) 1999-2006  Brian Paul   All Rights Reserved.
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a
  * copy of this software and associated documentation files (the "Software"),
  * to deal in the Software without restriction, including without limitation
  * the rights to use, copy, modify, merge, publish, distribute, sublicense,
  * and/or sell copies of the Software, and to permit persons to whom the
  * Software is furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included
  * in all copies or substantial portions of the Software.
  */
@@ -76,7 +76,7 @@ void GLInfo::initGL()
 {
 #if defined(__APPLE__)
     return;
-el#if defined(WIN32) || defined(_WIN32)
+#elif defined(WIN32) || defined(_WIN32)
     initWinGL();
 #elif defined(__linux__)
     initLuxGL();
@@ -98,11 +98,11 @@ const osg::ref_ptr<GLInfo> GLInfo::get()
         GLInfo* res = new GLInfo;
         inst = res;
     }
-    
+
 
     return inst;
 }
-    
+
 
 void GLInfo::updateLimits()
 {
@@ -110,7 +110,7 @@ void GLInfo::updateLimits()
         GLenum _token;
         const char* _name;
     };
-    
+
 #if defined(GL_ARB_vertex_shader)
     const struct token_name vertex_limits[] = {
         { GL_MAX_VERTEX_UNIFORM_COMPONENTS_ARB, "GL_MAX_VERTEX_UNIFORM_COMPONENTS_ARB" },
@@ -122,7 +122,7 @@ void GLInfo::updateLimits()
         { GL_MAX_TEXTURE_COORDS_ARB, "GL_MAX_TEXTURE_COORDS_ARB" },
         { (GLenum) 0, NULL }
     };
-    
+
 
     for ( int idx = 0; vertex_limits[idx]._token; idx++) {
         GLint max[1];
@@ -133,7 +133,7 @@ void GLInfo::updateLimits()
     }
 
 #endif
-    
+
 
 #if defined(GL_ARB_fragment_shader)
     const struct token_name fragment_limits[] = {
@@ -142,7 +142,7 @@ void GLInfo::updateLimits()
         { GL_MAX_TEXTURE_IMAGE_UNITS_ARB, "GL_MAX_TEXTURE_IMAGE_UNITS_ARB" },
         { (GLenum) 0, NULL }
     };
-    
+
 
     for ( int idx = 0; fragment_limits[idx]._token; idx++) {
         GLint max[1];
@@ -170,8 +170,8 @@ bool GLInfo::isOK() const
 {
     return !_glvendor.empty() && !_glversion.empty() && !_glrenderer.empty();
 }
-    
-    
+
+
 
 
 int GLInfo::getLimit( int intenum ) const
@@ -179,7 +179,7 @@ int GLInfo::getLimit( int intenum ) const
     for ( int idx=0; idx<_limits.size(); idx++ )
         if ( _limits[idx]._token==intenum )
             return _limits[idx]._value;
-    
+
 
     return -1;
 }
